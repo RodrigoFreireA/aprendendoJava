ef2bf08c921b2aec1363b72782178b85e27a9e39
Rodrigo <Rodrigo@Rodrigo-PC>
2025-03-21 11:54:06 -0300

    Initial Commmit

---
+++ duke.choice/src/main/java/duke/choice/duke/choice/DukeChoice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package duke.choice.duke.choice;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class DukeChoice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
