e46faeaaeea6b516309860d4003bfd422e12b39d
Rodrigo <Rodrigo@Rodrigo-PC>
2025-03-24 12:00:26 -0300

    --Second Test - Operators 2 --

--- duke.choice/src/main/java/duke/choice/ShopApp.java
+++ duke.choice/src/main/java/duke/choice/ShopApp.java
@@ -11,17 +11,21 @@ package duke.choice;
 public class ShopApp {
 
     public static void main(String[] args) {
+        
+        double tax = 0.2, total = 0.0;
         System.out.println("Hello World!");
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
         item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
-        item2.description = "Orange Jacket";
+        item2.description = "Orange T-Shirt";
         item2.price = 10.5;
         item2.size = "S";
         System.out.println("item1 = " + item1.description + "," + item1.price + "," + item1.size);
         System.out.println("item2 = " + item2.description + "," + item2.price + "," + item2.size);
-
+        //For this example, the user will buy 1 jacket and 2 T-shirts
+        total = (item1.price + (item2.price *2)) * (1+tax);
+        System.out.println("Total = " + total);
     }
 }
